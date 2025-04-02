3bda127593f118ff4593d2b0f95decbf53ececf2
Theuzindeveloper <131308267+Matheusleitelima@users.noreply.github.com>
2025-04-02 22:02:39 +0000

    Adicionando as habilidades

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,6 +2,21 @@
 
 #define TAMANHO_TABULEIRO 10
 #define TAMANHO_NAVIO 3
+#define TAMANHO_HABILIDADE 5
+
+int aplicarHabilidade(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna, int habilidade[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE]) {
+    int centro = TAMANHO_HABILIDADE / 2;
+    for (int i = 0; i < TAMANHO_HABILIDADE; i++) {
+        for (int j = 0; j < TAMANHO_HABILIDADE; j++) {
+            int x = linha + i - centro;
+            int y = coluna + j - centro;
+            if (x >= 0 && x < TAMANHO_TABULEIRO && y >= 0 && y < TAMANHO_TABULEIRO && habilidade[i][j] == 1) {
+                tabuleiro[x][y] = 5;
+            }
+        }
+    }
+    return 0;
+}
 
 int main() {
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
@@ -13,52 +28,54 @@ int main() {
         }
     }
 
-    
-    int navioHorizontal[TAMANHO_NAVIO] = {3, 3, 3};
-    int navioVertical[TAMANHO_NAVIO] = {3, 3, 3};
-    int navioDiagonal1[TAMANHO_NAVIO] = {3, 3, 3};
-    int navioDiagonal2[TAMANHO_NAVIO] = {3, 3, 3};
-
-    
-    int linhaNavio1 = 2, colunaNavio1 = 3; // Navio horizontal
-    int linhaNavio2 = 5, colunaNavio2 = 6; // Navio vertical
-    int linhaNavio3 = 1, colunaNavio3 = 1; // Navio diagonal para baixo
-    int linhaNavio4 = 1, colunaNavio4 = 8; // Navio diagonal para cima
-
-    // Verifica se os navios cabem no tabuleiro
-    if (colunaNavio1 + TAMANHO_NAVIO > TAMANHO_TABULEIRO ||
-        linhaNavio2 + TAMANHO_NAVIO > TAMANHO_TABULEIRO ||
-        linhaNavio3 + TAMANHO_NAVIO > TAMANHO_TABULEIRO ||
-        colunaNavio3 + TAMANHO_NAVIO > TAMANHO_TABULEIRO ||
-        linhaNavio4 + TAMANHO_NAVIO > TAMANHO_TABULEIRO ||
-        colunaNavio4 - TAMANHO_NAVIO < -1) {
-        printf("Erro: Navio fora do tabuleiro!\n");
-        return 1;
-    }
-
-    // Verifica sobreposição 
-    for (int i = 0; i < TAMANHO_NAVIO; i++) {
-        if (tabuleiro[linhaNavio1][colunaNavio1 + i] == 3 ||
-            tabuleiro[linhaNavio2 + i][colunaNavio2] == 3 ||
-            tabuleiro[linhaNavio3 + i][colunaNavio3 + i] == 3 ||
-            tabuleiro[linhaNavio4 + i][colunaNavio4 - i] == 3) {
-            printf("Erro: Navios se sobrepõem!\n");
-            return 1;
-        }
-    }
+    // Posições iniciais
+    int linhaNavio1 = 2, colunaNavio1 = 3;
+    int linhaNavio2 = 5, colunaNavio2 = 6;
+    int linhaNavio3 = 1, colunaNavio3 = 1;
+    int linhaNavio4 = 1, colunaNavio4 = 8;
 
     // Posiciona os navios 
     for (int i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[linhaNavio1][colunaNavio1 + i] = navioHorizontal[i];
-        tabuleiro[linhaNavio2 + i][colunaNavio2] = navioVertical[i];
-        tabuleiro[linhaNavio3 + i][colunaNavio3 + i] = navioDiagonal1[i];
-        tabuleiro[linhaNavio4 + i][colunaNavio4 - i] = navioDiagonal2[i];
+        tabuleiro[linhaNavio1][colunaNavio1 + i] = 3;
+        tabuleiro[linhaNavio2 + i][colunaNavio2] = 3;
+        tabuleiro[linhaNavio3 + i][colunaNavio3 + i] = 3;
+        tabuleiro[linhaNavio4 + i][colunaNavio4 - i] = 3;
     }
 
-    // 
-    printf("   A B C D E F G H I J\n"); 
+    // Matrizes das habilidades
+    int cone[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int cruz[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE] = {
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    int octaedro[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    // Aplicar habilidades
+    aplicarHabilidade(tabuleiro, 3, 3, cone);
+    aplicarHabilidade(tabuleiro, 6, 6, cruz);
+    aplicarHabilidade(tabuleiro, 8, 8, octaedro);
+
+    // Exibe o tabuleiro
+    printf("   A B C D E F G H I J\n");
     for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
-        printf("%2d ", i + 1); // Exibe os números das linhas
+        printf("%2d ", i + 1);
         for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
